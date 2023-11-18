f60c73559c26f3864be6b40921af959d34960250
Florian Thiel <flth@flth.de>
2023-11-18 20:42:54 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,26 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "germany", "packwagen", "gepäckwagen", "packages", "city bahn", },
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,22 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Luggage cart of the City-Bahn \"Expressdienst\"",
+		["mod_desc"] = "This mod contains the baggage car of the City-Bahn, which ran under the label \"Express Service\". The cars were painted in the City-Bahn design and ran in the City-Bahn trains.\n\nThe baggage car mod is required and therefore also the UIC-X coaches mod.",
+
+        ["expressdienst_name"] = "Luggage cart of the City-Bahn Dm903 (1984)",
+		["expressdienst_desc"] = "The Deutsche Bundesbahn also integrated luggage trolleys into the new city railway under the label \"Express Service\". These were often placed behind the control car or as a sandwich for the locomotive.",
+
+	},
+	de = {
+
+		["mod_name"] = "Gepäckwagen der City-Bahn \"Expressdienst\"",
+		["mod_desc"] = "Dieser Mod enthält den Gepäckwagen der City-Bahn die unter dem Label \"Expressdienst\" liefen. Die Wagen waren im City-Bahn-Design lackiert und liefen in den City-Bahn-Zügen.\n\nBenötigt wird der Packwagen-Mod und damit in Folge auch der UIC-X-Wagen-Mod (m-Wagen).",
+
+        ["expressdienst_name"] = "Gepäckwagen City-Bahn Dm903 (1984)",
+		["expressdienst_desc"] = "Bei der Deutschen Bundesbahn wurden in die neue City-Bahn auch Gepäckwagen integriert unter dem Label \"Expressdienst\". Diese wurden gerne hinter dem Steuerwagen eingereiht oder auch als Sandwich für die Lok.",
+
+	},
+}
+end
